c06917f7b94577d6e4f276bbe5b873f05d41a576
Jordi Hernandez <jjordihm9@gmail.com>
2021-11-08 16:20:30 +0100

    added status state to stopwatch hook

--- src/hooks/useStopWatch.ts
+++ src/hooks/useStopWatch.ts
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 
+enum Status {
+  Null = 'a',
+  Running = 'b',
+  Stopped = 'c'
+}
+
 export const useStopWatch = () => {
+  const [status, setStatus] = useState(Status.Null);
   const [time, setTime] = useState(0);
   const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
 
   const start = (): void => {
+    setStatus(Status.Running);
     setTimer(setInterval(() => {
       setTime(time => time + 10);
     }, 10));
   }
 
   const stop = (): void => {
+    setStatus(Status.Stopped);
     if (timer !== null) {
       clearInterval(timer);
       setTimer(null);
@@ -18,6 +27,7 @@ export const useStopWatch = () => {
   }
 
   return {
+    status,
     time,
     start,
     stop
